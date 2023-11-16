b18672f2337dd30a979b41ea1510c6a83878042a
MenY_ED <busayn@gmail.com>
2023-11-16 22:57:23 +0300

    Added a json converter that serializes a field object as a string.

---
+++ StarfallAfterlifeBridge/Serialization/ObjectToJsonStringConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace StarfallAfterlife.Bridge.Serialization
+{
+    public class ObjectToJsonStringConverter<T> : JsonConverter<T> where T : class
+    {
+        public override bool CanConvert(Type typeToConvert)
+        {
+            if (typeToConvert == typeof(string))
+                return false;
+
+            return base.CanConvert(typeToConvert);
+        }
+        public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var text = JsonSerializer.Deserialize<string>(ref reader, options);
+            return JsonHelpers.DeserializeUnbuffered<T>(text, options);
+        }
+
+        public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+        {
+            var text = JsonHelpers.SerializeUnbuffered(value, options);
+            JsonSerializer.Serialize(writer, text, options);
+        }
+    }
+}
