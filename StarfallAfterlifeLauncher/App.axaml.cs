bd1ea220ec3ebb1651808754d86f9b6dc032554d
MenY_ED <busayn@gmail.com>
2023-12-22 14:17:46 +0300

    Added a base project for the new launcher.

---
+++ StarfallAfterlifeLauncher/App.axaml.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using StarfallAfterlife.Launcher.Views;
+
+namespace StarfallAfterlife.Launcher;
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = new MainWindow
+            {
+
+            };
+        }
+        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
+        {
+            singleViewPlatform.MainView = new MainView
+            {
+
+            };
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
