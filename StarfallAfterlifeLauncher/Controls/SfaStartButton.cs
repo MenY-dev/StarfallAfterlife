27ae2f8c0fd1a59a94932450b19fcb710b985ae7
MenY_ED <busayn@gmail.com>
2024-01-02 14:01:01 +0300

    Added a new game start button.

---
+++ StarfallAfterlifeLauncher/Controls/SfaStartButton.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarfallAfterlife.Launcher.Controls
+{
+    public class SfaStartButton : Button
+    {
+        protected override Type StyleKeyOverride => typeof(SfaStartButton);
+    }
+}
